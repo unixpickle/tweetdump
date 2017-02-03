a07f1913d1c184b582130fced8a881f834193ebb
Alex Nichol <unixpickle@gmail.com>
2017-02-03 11:41:13 -0500

    basic stream dumping works

---
+++ README.md
@@ -0,0 +1,3 @@
+# tweetdump
+
+This project is designed to dump millions of tweets to a CSV file. What you do with the data is up to you, but I plan to use it for unsupervised learning (see [tweetenc](https://github.com/unixpickle/tweetenc)).
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/unixpickle/essentials"
+)
+
+func main() {
+	var consumerKey, consumerSecret string
+	var accessToken, accessSecret string
+	var language string
+	flag.StringVar(&consumerKey, "consumer-key", "", "consumer key")
+	flag.StringVar(&consumerSecret, "consumer-secret", "", "consumer secret")
+	flag.StringVar(&accessToken, "access-token", "", "access token")
+	flag.StringVar(&accessSecret, "access-secret", "", "access secret")
+	flag.StringVar(&language, "language", "en", "tweet language filter")
+	flag.Parse()
+	if consumerKey == "" || consumerSecret == "" || accessToken == "" || accessSecret == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	params := &twitter.StreamSampleParams{
+		StallWarnings: twitter.Bool(true),
+		Language:      []string{language},
+	}
+	stream, err := client.Streams.Sample(params)
+	if err != nil {
+		essentials.Die("streaming:", err)
+	}
+
+	writer := csv.NewWriter(os.Stdout)
+	for msg := range stream.Messages {
+		if tweet, ok := msg.(*twitter.Tweet); ok {
+			writer.Write([]string{tweet.IDStr, tweet.User.ScreenName, tweet.Text})
+			writer.Flush()
+		}
+	}
+}
